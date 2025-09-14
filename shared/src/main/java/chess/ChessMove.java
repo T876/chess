aacf6a46effdb25072989e3d8bf8c1af59f57ec3
T876 <wondertate@gmail.com>
2025-09-13 19:56:11 -0600

    P0: String overrides for ChessMove and ChessPosition

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -42,4 +42,9 @@ public class ChessMove {
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
     }
+
+    @Override
+    public String toString() {
+        return String.format("S:%s E:%s", startPosition, endPosition);
+    }
 }
