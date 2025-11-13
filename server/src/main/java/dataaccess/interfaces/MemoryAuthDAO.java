cd828f347650a246e7bb8e863314f96521d29093
T876 <wondertate@gmail.com>
2025-11-12 21:51:01 -0700

    P3: Memory auth DAO skeleton

---
+++ server/src/main/java/dataaccess/interfaces/MemoryAuthDAO.java
@@ -0,0 +1,28 @@
+package dataaccess.interfaces;
+
+
+import dataaccess.DataAccessException;
+import model.AuthData;
+
+import java.util.UUID;
+
+public class MemoryAuthDAO implements  IAuthDAO{
+    public MemoryAuthDAO() {
+
+    };
+
+    public AuthData makeAuthData(String username) {
+        AuthData newAuth = new AuthData(username, UUID.randomUUID().toString());
+        return newAuth;
+    };
+
+    public boolean verifyAuthToken(String authToken) throws DataAccessException {
+        return true;
+    };
+
+    public void logout(String authToken) throws DataAccessException {
+
+    }
+
+    public void clear() {};
+}
