a0ff35ed5d8aef27aa35124fa13bb38dbd9e2a92
T876 <wondertate@gmail.com>
2025-11-14 17:57:08 -0700

    P3: No duplicate users

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -5,13 +5,26 @@ import model.UserData;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MemoryUserDAO implements IUserDAO {
     List<UserData> users = new ArrayList<>();
     public MemoryUserDAO() {};
 
     public void createUser(UserData data) throws DataAccessException{
+        if (userAlreadyExists(data.username())) {
+            throw new DataAccessException("Error: already taken");
+        }
         users.add(data);
+    };
+
+    boolean userAlreadyExists(String username) {
+        for(UserData user :users) {
+            if (Objects.equals(user.username(), username)) {
+                return true;
+            }
+        }
+        return false;
     };
 
     public boolean verifyUser(String username, String password) throws DataAccessException {
