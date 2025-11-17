c39a8ea382f5f6566cc4e140eaef632a384e7d73
T876 <wondertate@gmail.com>
2025-11-16 23:48:02 -0700

    P4: Create user tests

--- server/src/test/java/dataaccess/GameDAOTests.java
+++ server/src/test/java/dataaccess/GameDAOTests.java
@@ -1,4 +1,47 @@
 package dataaccess;
 
+import dataaccess.interfaces.IUserDAO;
+import model.UserData;
+import org.junit.jupiter.api.*;
+
 public class GameDAOTests {
+    private static IUserDAO userDAO;
+
+    @BeforeAll
+    public static void init() {
+        try{
+            userDAO = new SQLUserDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    public void setup() {
+        userDAO.clear();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Create User Success")
+    public void createUserSuccess() {
+        Assertions.assertDoesNotThrow(() -> {
+            userDAO.createUser(new UserData("username", "password", "email"));
+        });
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Create User Fails Duplicate")
+    public void createUserDuplicate() {
+        try{
+            userDAO.createUser(new UserData("username", "password", "email"));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            userDAO.createUser(new UserData("username", "password", "email"));
+        });
+    }
 }
