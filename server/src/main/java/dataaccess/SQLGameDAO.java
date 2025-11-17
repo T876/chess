1240f78b7eb37103fcc8558c0c5b5346b1fa34f5
T876 <wondertate@gmail.com>
2025-11-16 18:30:33 -0700

    P4: Sql get all games

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,5 +1,7 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import dataaccess.interfaces.IGameDAO;
 import model.GameData;
 
@@ -9,9 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SQLGameDAO implements IGameDAO {
+    Gson serializer;
 
     public SQLGameDAO() throws DataAccessException {
         this.setupTable();
+        this.serializer = new Gson();
     };
 
     // Create
@@ -21,8 +25,31 @@ public class SQLGameDAO implements IGameDAO {
 
     // Read
     public List<GameData> getAllGames(){
-        return new ArrayList<>();
+        List<GameData> games = new ArrayList<>();
+        String queryString = "SELECT id, whiteUsername, blackUsername, gameName, chessGame FROM game";
+        try (Connection c = DatabaseManager.getConnection()) {
+            try (var query = c.prepareStatement(queryString)){
+                try(var result = query.executeQuery()) {
+                    while(result.next()) {
+                        String gameJson = result.getString("chessGame");
+                        games.add(new GameData(
+                                result.getInt("id"),
+                                result.getString("whiteUsername"),
+                                result.getString("blackUsername"),
+                                result.getString("gameName"),
+                                serializer.fromJson(gameJson, ChessGame.class)
+                        ));
+                    }
+                };
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        return games;
     };
+
+
 
     // Update
     public void joinGame(String color, int gameID, String username) throws DataAccessException {
@@ -30,7 +57,16 @@ public class SQLGameDAO implements IGameDAO {
     };
 
     // Delete
-    public void clear() { };
+    public void clear() {
+        String queryString = "TRUNCATE TABLE game";
+        try (Connection c = DatabaseManager.getConnection()) {
+            try (var query = c.prepareStatement(queryString)){
+                query.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    };
 
     private void setupTable() throws DataAccessException {
         DatabaseManager.createDatabase();
