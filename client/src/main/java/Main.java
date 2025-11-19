ad568108a0b9cbcaef28bc86e9960952f6de64a8
T876 <wondertate@gmail.com>
2025-11-19 16:34:24 -0700

    P5: Basic user input

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        // Services and Utils
+        Scanner inputScanner = new Scanner(System.in);
+
         System.out.println("♕ 240 Chess Client ♕");
+
+        System.out.println("Enter Your Name: ");
+        System.out.print(">>> ");
+
+        String name = inputScanner.next();
+
+        System.out.println("Welcome " + name);
     }
 }
