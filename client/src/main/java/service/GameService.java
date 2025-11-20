3580912c34df9d026e9f16d42f3f8bde6e36f815
T876 <wondertate@gmail.com>
2025-11-19 20:57:35 -0700

    P5: stub out client side service methods

--- client/src/main/java/service/GameService.java
+++ client/src/main/java/service/GameService.java
@@ -1,11 +1,29 @@
 package service;
 
+import chess.ChessGame;
+import model.GameData;
 import server.ServerFacade;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameService {
     private ServerFacade server;
+    public ChessGame selectedGame;
 
     public GameService(ServerFacade server) {
         this.server = server;
+    }
+
+    public boolean createGame(GameData data, String authToken) {
+        return true;
+    }
+
+    public List<GameData> listGames(String authToken) {
+        return new ArrayList<>();
+    }
+
+    public GameData joinGame(boolean observeOnly, String authToken) {
+        return new GameData(1, null, null, "game1", new ChessGame());
     }
 }
--- client/src/main/java/service/UserService.java
+++ client/src/main/java/service/UserService.java
@@ -1,11 +1,28 @@
 package service;
 
+import model.AuthData;
 import server.ServerFacade;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class UserService {
     private ServerFacade server;
+    public AuthData authData;
 
     public UserService(ServerFacade server) {
         this.server = server;
+    }
+
+    public void register(String username, String password, String email) {
+        this.authData = new AuthData(username, UUID.randomUUID().toString());
+    }
+
+    public void login(String username, String password) {
+        this.authData = new AuthData(username, UUID.randomUUID().toString());
+    }
+
+    public void logout() {
+        this.authData = null;
     }
 }
