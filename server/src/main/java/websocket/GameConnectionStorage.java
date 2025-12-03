e8e3d3ea0554a3950bd66c3e72682ea526309acf
T876 <wondertate@gmail.com>
2025-12-02 20:54:47 -0700

    P6: minor cleanup

--- server/src/main/java/websocket/GameConnectionStorage.java
+++ server/src/main/java/websocket/GameConnectionStorage.java
@@ -14,15 +14,10 @@ public class GameConnectionStorage {
     public void add(int gameId, Session session) {
         String gameIDstring = Integer.toString(gameId);
         connections.computeIfAbsent(gameIDstring, list -> new ArrayList<>()).add(session);
-        return;
     }
 
     public void remove(int gameId, Session session) {
         connections.get(Integer.toString(gameId)).remove(session);
-    }
-
-    public void endGame(int gameId) {
-        connections.remove(Integer.toString(gameId));
     }
 
     public void broadcastToGame(int gameID, String message, Session sender, boolean includeSender) throws IOException {
